09578bdf8d9e9802387494f2b98183c851c8052a
Motoki1996 <145560101+Motoki1996@users.noreply.github.com>
2023-12-03 10:15:35 +0900

    update

--- example-app/routes/api.php
+++ example-app/routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', LoginController::class)->name('login'); 
+Route::post('/logout', LogoutController::class)->name('logout'); 
+
+Route::middleware('auth:sanctum')->group(function () {
+    //認証を必要とする機能を記載
+    // Route::apiResource('tasks', 'App\Http\Controllers\TaskController');
+    // Route::patch('tasks/update-done/{task}', 'App\Http\Controllers\TaskController@updateDone');
+    Route::get('user', function (Request $request) {
+        return $request->user();
+    });
 });
